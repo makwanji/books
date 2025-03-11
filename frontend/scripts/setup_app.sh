2ba9c5882385bc48be09fcfa51160197b21bda93
jignesh-makwana_adn <jignesh.makwana@adnovum.com>
2025-03-11 17:26:06 +0800

    Updated build script

--- frontend/scripts/setup_app.sh
+++ frontend/scripts/setup_app.sh
@@ -3,6 +3,8 @@ git clone https://github.com/makwanji/books.git
 cd books/frontend
 npm install
 cp .env.sample .env
-sudo npm run build
-sudo cp -r build/* /var/www/html
+npm run build
+sudo mv /var/www/html /var/www/html.orig
+sudo ln -s ~/books/frontend/build /var/www/html
+sudo gpasswd -a www-data adminuser
 sudo systemctl restart nginx
